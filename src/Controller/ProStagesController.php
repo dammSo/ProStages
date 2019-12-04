357b49fe1b51f6e0a533f8fe0d2e3adb1a92c482
Damien Serrano <dserrano001@iutbayonne.univ-pau.fr>
2019-12-04 09:27:46 +0100

    Définition des routes dans le contrôleur

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -8,12 +8,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/pro/stages", name="pro_stages")
+     * @Route("/", name="ProStages_acceuil")
      */
     public function index()
     {
-        return $this->render('pro_stages/index.html.twig', [
+        return $this->render('ProStages/index.html.twig', [
             'controller_name' => 'ProStagesController',
         ]);
     }
+   /**
+     * @Route("/entreprises", name="ProStages_entreprises")
+     */
+    public function page_entreprises()
+    {
+        return $this->render('ProStages/page_entreprises.html.twig', [
+            'controller_name' => 'ProStagesController',
+        ]);
+    }
+    /**
+     * @Route("/formations", name="ProStages_formations")
+     */
+    public function page_formations()
+    {
+        return $this->render('ProStages/page_formations.html.twig', [
+            'controller_name' => 'ProStagesController',
+        ]);
+    }
+    /**
+     * @Route("/stages/{id}", name="ProStages")
+     */
+    public function page_stageAvecId($id)
+    {
+        return $this->render('ProStages/page_stageAvecId.html.twig', [
+            'controller_name' => 'ProStagesController', 'id' => $id
+        ]);
+    }
+
 }
